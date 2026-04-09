249402d172fefa73e1d7a2d3ff4432864776bfc9
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2026-04-09 12:31:31 +0530

    refactor styles in TodoHeader component for improved layout and responsiveness

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -19,5 +19,38 @@ import { RouterLink } from 'vue-router';
 </template>
 
 <style lang="scss" scoped>
+header {
+  background-color: #f1f1f1;
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 25px 16px;
 
+    .branding {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      img {
+        max-width: 50px;
+      }
+
+      h1 {
+        font-size: 24px;
+      }
+    }
+
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 12px;
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+}
 </style>
